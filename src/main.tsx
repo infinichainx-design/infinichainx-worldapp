7765e0f24d8e7eb0591b8e0d7d3d61bc93beacc1
infinichainx-design <infinichainx@gmail.com>
2025-10-17 07:01:48 +0200

    Infra: add main.tsx as Vite entry point to mount App and finalize routing flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css"; // Asegúrate de tener estilos base si los necesitas
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
