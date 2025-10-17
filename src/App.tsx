25670626e42e0d97248143aecaf45b3640d8dc4e
infinichainx-design <infinichainx@gmail.com>
2025-10-17 03:55:17 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Swap } from "../components/Swap";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>InfinichainX World App</h1>
+      <Swap />
+    </div>
+  );
+}
+
+export default App;
